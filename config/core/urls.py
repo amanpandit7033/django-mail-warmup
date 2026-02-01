5908ba4271ba01bb079bc0cd65c77a5705d3909b
Aman Pandit <amanpandit7033@gmail.com>
2026-02-01 21:44:16 +0530

    Integrate Tailwind CLI with Django static files (remove CDN)

--- config/core/urls.py
+++ config/core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
@@ -14,3 +16,6 @@ urlpatterns = [
     path('warmup/', warmup_control, name='warmup_control'),
     path('upload/<str:type>/', upload_csv, name='upload_csv'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
